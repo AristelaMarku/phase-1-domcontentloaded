b9181a545c9b8fd337d40b1feddb1e529f708ad5
Aristela Marku <aristela.marku@gmail.com>
2021-09-25 22:51:21 +0200

    DOMContentLoadedlab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded',function (e){
+   const paragrap=document.getElementById('text')
+    paragrap.innerHTML="This is really cool!"
+})
